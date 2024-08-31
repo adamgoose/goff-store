14cc04c7fd52ec35ca520ee2dcc958ba4edcfc02
Adam Engebretson <adam@enge.me>
2024-08-31 02:21:23 -0500

    Initial Commit

---
+++ src/goff.ts
@@ -0,0 +1,43 @@
+export type FeatureFlag<T> = {
+  variations: Record<string, Variation<T>>;
+  targeting?: Rule[];
+  defaultRule: Rule;
+  scheduled?: ScheduledStep<T>[];
+  experimentation?: Experimentation;
+  metadata?: Record<string, any>;
+  trackEvents?: boolean;
+  disable?: boolean;
+  version?: string;
+  converter?: string;
+};
+
+export type Variation<T> = T;
+
+export type Rule = {
+  name: string;
+  query: string;
+  variation: string;
+  percentage: Record<string, number>;
+  progressiveRollout?: ProgressiveRollout;
+  disable?: boolean;
+};
+
+export type Experimentation = {
+  start: Date;
+  end: Date;
+};
+
+export type ScheduledStep<T> = FeatureFlag<T> & {
+  date: Date;
+};
+
+export type ProgressiveRollout = {
+  initial?: ProgressiveRolloutStep;
+  end?: ProgressiveRolloutStep;
+};
+
+export type ProgressiveRolloutStep = {
+  variation: string;
+  percentage: number;
+  date: Date;
+};
